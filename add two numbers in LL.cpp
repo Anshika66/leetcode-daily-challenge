741fac2d3555ad1f3db18c0e39d7fbf38179c21d
Anshika Srivastav <231210021@nitdelhi.ac.in>
2026-04-21 00:47:49 +0530

    Refactor addTwoNumbers to use helper function

--- add two numbers in LL.cpp
+++ add two numbers in LL.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummyHead = new ListNode(0);
-        ListNode* tail = dummyHead;
-        int carry = 0;
-
-        while (l1 != nullptr || l2 != nullptr || carry != 0) {
-            int digit1 = (l1 != nullptr) ? l1->val : 0;
-            int digit2 = (l2 != nullptr) ? l2->val : 0;
-
-            int sum = digit1 + digit2 + carry;
-            int digit = sum % 10;
-            carry = sum / 10;
-
-            ListNode* newNode = new ListNode(digit);
-            tail->next = newNode;
-            tail = tail->next;
-
-            l1 = (l1 != nullptr) ? l1->next : nullptr;
-            l2 = (l2 != nullptr) ? l2->next : nullptr;
+    void insertAtTail(ListNode* &head, ListNode* &tail, int digit){
+        ListNode* temp = new ListNode(digit);
+        
+        if(head == NULL){
+            head = temp;
+            tail = temp;
+        } else {
+            tail->next = temp;
+            tail = temp;
         }
-
-        ListNode* result = dummyHead->next;
-        delete dummyHead;
-        return result;
+    }
+    
+    ListNode* add(ListNode* first, ListNode* second){
+        int carry = 0;
+        ListNode* anshead = NULL;
+        ListNode* anstail = NULL;
+        
+        while(first != NULL || second != NULL || carry != 0){
+            int val1 = (first != NULL) ? first->val : 0;
+            int val2 = (second != NULL) ? second->val : 0;
+            
+            int sum = carry + val1 + val2;
+            int digit = sum % 10;
+            insertAtTail(anshead, anstail, digit);
+            carry = sum / 10;
+            
+            if(first != NULL) first = first->next;
+            if(second != NULL) second = second->next;
+        }
+        
+        return anshead;
+    }
+    
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        
+        ListNode* ans = add(l1, l2);
+        
+        
+        
+        return ans;
     }
 };
