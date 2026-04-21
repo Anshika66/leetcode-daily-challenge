1d33b1dcbfefe733e5c87a990741d068009c4577
Anshika Srivastav <231210021@nitdelhi.ac.in>
2026-04-21 18:27:59 +0530

    Implement deep copy for list with random pointers

--- 138. Copy List with Random Pointer.cpp
+++ 138. Copy List with Random Pointer.cpp
@@ -40,3 +40,67 @@ public:
         
     }
 };
+
+
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    void insertCopyInBetween(Node* head){
+        Node* temp = head;
+        while(temp != NULL){
+            Node* copyNode = new Node(temp->val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp = temp->next->next;
+        }
+    }
+    void connectRandomPointers(Node* head){
+        Node* temp = head;
+        while(temp != NULL){
+            Node* copyNode = temp->next;
+            if(temp->random){
+                copyNode->random = temp->random->next;
+            }else{
+                copyNode->random = NULL;
+            }
+            temp = temp->next->next;
+        }
+    }
+    Node* getdeepcopylist(Node* head){
+        Node* dummyNode = new Node(-1);
+        Node* res = dummyNode;
+        Node* temp = head;
+
+        while(temp != NULL){
+            res->next = temp->next;
+            res = res->next ;
+
+            temp->next=temp->next->next;
+            temp= temp->next;
+        }
+        return dummyNode->next;
+    }
+    Node* copyRandomList(Node* head) {
+        insertCopyInBetween(head);
+        connectRandomPointers(head);
+        return getdeepcopylist(head);
+
+    }
+};
