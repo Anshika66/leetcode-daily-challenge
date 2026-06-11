25e5bee95b91f77fe33d430e05cc9f37a0b73501
Anshika Srivastav <231210021@nitdelhi.ac.in>
2026-06-11 11:32:28 +0530

    Add character replacement solution in C++
    Implement character replacement algorithm to find the length of the longest substring with repeating characters after at most k replacements.

---
+++ 424. Longest Repeating Character Replacement.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0;
+        int r = 0;
+        int maxlen = 0;
+        int maxf = 0;
+        unordered_map<char , int > hash;
+
+        while(r < s.size()){
+            hash[s[r]-'A']++;
+            maxf = max(maxf ,hash[s[r]-'A']);
+            if((r-l+1) - maxf > k){
+                hash[s[l] - 'A']--;
+                maxf = 0;
+                l= l +1;
+            }
+            if((r-l+1)-maxf <= k){
+                maxlen = max(maxlen , r-l+1);
+            }
+            r++;
+        }
+
+        return maxlen ;
+        
+    }
+};
