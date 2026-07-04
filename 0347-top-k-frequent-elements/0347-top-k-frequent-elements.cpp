74bbda76ccf5531297e44f59a3ce5ee7f933a99c
Anshika Srivastav <231210021@nitdelhi.ac.in>
2026-07-04 10:57:44 +0530

    Time: 1 ms (64.82%), Space: 17.9 MB (84.44%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    struct cmp {
+        bool operator()(const pair<int,int>& a,
+                        const pair<int,int>& b) const {
+            return a.first > b.first;   // Min Heap
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        priority_queue<pair<int, int> , vector<pair<int, int>> , cmp > pq;
+        unordered_map<int , int> f;
+        for(int i = 0; i< n ; i++){
+            f[nums[i]]++;
+        }
+
+        for(auto i : f){
+            int element = i.first ; 
+            int freq = i.second;
+
+           pair<int , int> curr = {freq , element};
+
+           if(pq.size() <k){
+            pq.push(curr);
+            continue;
+           }
+
+           if(curr.first < pq.top().first){
+            continue;
+           }else {
+            pq.pop();
+            pq.push(curr);
+           }
+        }
+
+        vector<int> ans; 
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
